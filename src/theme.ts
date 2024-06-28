0cf8a40732d8f2f146d688acf2495e051d8ef655
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-06-28 17:16:04 +0900

    DefaultTheme Interface 설정
    darkTheme과 DefaultTheme에게 올바른 타입을 전달하기 위해 인터페이스 생성

--- src/theme.ts
+++ src/theme.ts
@@ -3,11 +3,11 @@ import { DefaultTheme } from "styled-components";
 export const lightTheme: DefaultTheme = {
     bgColor: "white",
     textColor: "black",
-    btnColor: "tomato",
+    accentColor: "tomato",
 };
 
 export const darkTheme: DefaultTheme = {
-    bgColor: "black",
-    textColor: "white",
-    btnColor: "teal",
+    bgColor: "#2f3640",
+    textColor: "#f5f6fa",
+    accentColor: "#4cd137",
 };
