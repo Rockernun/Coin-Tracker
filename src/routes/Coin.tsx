0b6709234d2e549bf7f7579a6183ae7f8f9ecd59
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-01 23:19:23 +0900

    Getting State (by using useLocation hook)
    Coins 화면에서 object를 통해 보낸 데이터, 즉 location object를 useLocation hook을 이용해서 얻어올 수 있다. 하지만, 여기서 state라는 속성이 unknown이기 때문에 name이 존재하지 않는다고 나온다.
    
    따라서 state가 어떻게 생겼는지 RouteState Interface를 통해 설명해줄 필요가 있었다.
    
    state가 정의되었으니 타이틀에 coin의 name을 직접 렌더링 시켜줄 수 있게 된 것이다.
    
    정리하자면, Coins 페이지에서 각 코인을 클릭해서 이동할 때, state가 Home에서 다른 화면으로 보내지는 것이다. coin 페이지에 직접 방문하면 name이 undefined라고 문제가 발생하는데 바로 state가 만들어지지 못했기 때문에 state가 존재하면 name을 가져오고, 그렇지 않다면 "Loading..."를 보여주도록 처리했다.

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,14 +1,54 @@
 //  Individual Coin Page
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface RouteParams {
     coinId: string;
 }
 
+interface RouteState {  //  state가 어떻게 생겼는지 알려줘야 한다.
+    name:string;
+}
+
+const Header = styled.div`
+    height:15vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display:block;
+`;
+
+const Title = styled.h1`
+    font-size:48px;
+    color:${(props) => props.theme.accentColor};
+`;
+
+const Container = styled.div`  
+    padding: 0px 20px;
+    max-width:480px;
+    margin: 0 auto;
+`;
+
 function Coin() {
-    const { coinId } = useParams<RouteParams>();  //  URL의 파라미터들을 가져오기
-    return <h1>Coin : {coinId}</h1>
+    const [loading, setLoading] = useState(true);
+    const { coinId } = useParams<RouteParams>();  //  Coin에서는 coinId를 받아서 파라미터로 사용할거다.
+    const {state} = useLocation<RouteState>();  //  react router DOM이 보내주는 location object에 접근하기만 하면 된다.
+    //  가져온 state로 타이틀에 coin의 name을 직접 뿌려줄 수 있다.
+    //  하지만, 다이렉트로 Coin 페이지로 접속하면 에러가 발생! state가 생성되려면 Home 화면을 먼저 열어야 하고, 내가 클릭할 때 state가 만들어지기 때문이다.
+    console.log(state.name);
+    return (
+      <Container>
+        <Header>
+          <Title>{state?.name || "Loading..."}</Title>  
+        </Header>
+        {loading ? (<Loader>Loading...</Loader>) : null}
+      </Container>
+    );
 }
 
 export default Coin;
