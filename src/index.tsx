1337924ee960db86d6e0c902d8811d4b341c0cb7
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-06 16:07:04 +0900

    Setting React Query

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@ import App from './App';
 import { darkTheme } from "./theme";
 import { ThemeProvider } from 'styled-components';
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={darkTheme}>
         <App />
       </ThemeProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
